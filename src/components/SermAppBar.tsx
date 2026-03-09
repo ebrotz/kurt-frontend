1dc3c0103d6b4b9ec099b0573e5029f6add50669
Edward Brotz <edward.brotz@gmail.com>
2026-03-09 13:56:05 -0700

    Add drawer

--- src/components/SermAppBar.tsx
+++ src/components/SermAppBar.tsx
@@ -1,14 +1,44 @@
-import { AppBar, IconButton, Toolbar } from '@mui/material'
+import { AppBar, Box, Drawer, IconButton, List, ListItemButton, ListItemText, Toolbar } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
+import React from 'react'
 
 export default function SermAppBar() {
+	const [isDrawerOpen, setDrawerOpen] = React.useState(false)
+
+	const toggleDrawer = () => {
+		setDrawerOpen(!isDrawerOpen)
+	};
+
+	const closeDrawer = () => {
+		setDrawerOpen(false)
+	}
+
 	return (
-		<AppBar>
-			<Toolbar>
-				<IconButton>
-					<MenuIcon />
-				</IconButton>
-			</Toolbar>
-		</AppBar>
+		<Box>
+			<AppBar>
+				<Toolbar>
+					<IconButton onClick={toggleDrawer}>
+						<MenuIcon />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+			<nav>
+				<Drawer open={isDrawerOpen} onClose={closeDrawer}>
+					<Box>
+						<List>
+							{
+								// Items should ideally be dynamic
+								['Services', 'Deployments', 'Incidents'].map((item, idx) => (
+									<ListItemButton key={idx}>
+										<ListItemText primary={item} />
+									</ListItemButton>
+								))
+							}
+						</List>
+					</Box>
+
+				</Drawer>
+			</nav>
+		</Box>
 	)
 }
